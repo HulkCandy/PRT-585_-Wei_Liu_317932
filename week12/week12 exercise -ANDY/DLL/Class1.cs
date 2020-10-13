012942589da2b75fb4f45687005b137861906dd1
HulkCandy <48338488+HulkCandy@users.noreply.github.com>
2020-10-13 09:32:52 +0930

    week 12 tutorial

---
+++ week12/week12 exercise -ANDY/DLL/Class1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DLL
+{
+    //https://medium.com/oppr/net-core-using-entity-framework-core-in-a-separate-project-e8636f9dc9e5
+    //https://itsallinthecode.com/ef-core-in-a-separate-class-library/
+
+    public class DALContext : DbContext
+    {
+        public DALContext(DbContextOptions<DALContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+    }
+}
